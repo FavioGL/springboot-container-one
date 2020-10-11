998985246db06ab37c84ae816f8091d4616bb50f
Favio <hugofavio@gmail.com>
2020-10-11 08:41:28 -0700

    Initial commit

---
+++ src/test/java/org/breakbot/containerone/ContainerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.breakbot.containerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
